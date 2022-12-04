ae680d5101a12bd64be8b0a3a79c197fc162d5c1
bluesky0906 <loslonbervene.1923@icloud.com>
2022-12-04 13:25:30 +0900

    fix SinRNN

--- app/UnTyped/SinRNN.hs
+++ app/UnTyped/SinRNN.hs
@@ -62,28 +62,27 @@ main = do
       cos_t = fmap (cos_wave step_per_cycle) times
       cos_t1 = tail cos_t ++ [cos_wave step_per_cycle (step_per_cycle * number_of_cycles)]
 
-  initModel <- sample $ SinRNNSpec (Device CPU 0) 2
+  initModel <- sample $ SinRNNSpec (Device CPU 0) 100
   -- | setting
   let numEpochs = 100
-      -- optim = mkAdam 0.001 0.9 0.999 (flattenParameters initModel)
-      optim = GD
+      optim = mkAdam 1 0.9 0.999 (flattenParameters initModel)
+      -- optim = GD
   plotWave "sin" (times, sin_t) (times, sin_t1)
 
   ((trainedModel, _), losses) <- mapAccumM [1..numEpochs] (initModel, optim) $ \epoc (model, opt) -> do
-    let x_tensor = fmap asTensor sin_t
-        t_tensor = fmap asTensor sin_t1
-        prediction = sinRNN model x_tensor
-        loss = mseLoss (stack (Dim 0) prediction) (stack (Dim 0) t_tensor)
-
-    (newParam, newOp) <- runStep model opt loss 5e-2
+    let x_tensor = fmap ((toDType Float) . reshape [1] . asTensor) sin_t
+        t_tensor = fmap ((toDType Float) . reshape [1] . asTensor) sin_t1
+        prediction = fmap (reshape [1]) (sinRNN model x_tensor)
+        loss = mseLoss (cat (Dim 0) prediction) (cat (Dim 0) t_tensor)
+    updated <- runStep model opt loss 1e-2
     print $ "Training Loss: " ++ (show loss)
-    return ((newParam, newOp), loss)
+    return (updated, loss)
 
-  let x_tensor = fmap asTensor sin_t
-      pred = fmap toDouble (sinRNN trainedModel x_tensor)
+  let x_tensor = fmap ((toDType Float) . reshape [1] . asTensor) sin_t
+      pred = fmap toDouble $ sinRNN trainedModel x_tensor
   plotWave "untyped-sinRNN_pred" (times, sin_t1) (times, pred)
 
-  let x_tensor = fmap asTensor cos_t
-      pred = fmap toDouble (sinRNN trainedModel x_tensor)
-  plotWave "untyped-inRNN_eval" (times, cos_t1) (times, pred)
+  let x_tensor = fmap ((toDType Float) . reshape [1] . asTensor) cos_t
+      pred = fmap toDouble $ sinRNN trainedModel x_tensor
+  plotWave "untyped-sinRNN_eval" (times, cos_t1) (times, pred)
   return ()
